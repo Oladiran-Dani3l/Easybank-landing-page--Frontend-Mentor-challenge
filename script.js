b64966fd107f1bbb169f22b0b0f9814a393fe467
Oladiran-Dani3l <100717659+Oladiran-Dani3l@users.noreply.github.com>
2024-02-13 13:16:39 +0100

    Comp;eted the landing page for the frontend mentor's challenge. made use of css flexbox as well as css grid and optimised the website for resposiveness for both tablet and mobile devices.

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 const hamburgerMenu = document.querySelector('#hmburger-menu')
 
+const BodyElement = document.querySelector('body')
+
+
 const headerElement = document.querySelector('#header')
 
 const overlayElement = document.querySelector('#overlayElement')
 
+const mobileLinks = document.querySelector('.mobile-links')
+
+
+//Toggling the menu and close button
 hamburgerMenu.addEventListener('click', () => {
 
     console.log("Open sesame")
 
     headerElement.classList.toggle('toggle-menu')
+    BodyElement.classList.toggle('no-scroll')
     overlayElement.classList.add('overlay')
     
+    fadeInOut(overlayElement)
+    fadeInOut(mobileLinks)
+    
+})
+
+//Function to control animation of mobile menu links appearance
+function fadeInOut(element) {
     if (headerElement.classList.contains('toggle-menu')) {
         console.log('Yesirrrrr')
-        overlayElement.classList.add('fade-in')
-        overlayElement.classList.remove('fade-out')
+        element.classList.add('fade-in')
+        element.classList.remove('fade-out')
     } else {
         console.log('Fade-out')
-        overlayElement.classList.add('fade-out')
-        overlayElement.classList.remove('fade-in')
+        element.classList.add('fade-out')
+        element.classList.remove('fade-in')
     }
-})
+}
